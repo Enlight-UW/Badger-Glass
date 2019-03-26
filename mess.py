103300fa35c5a80423cccbcc8c818f3dce334544
Vinay <vjanard@gmail.com>
2019-03-26 23:13:31 +0000

    asdf

--- mess.py
+++ mess.py
@@ -2,16 +2,25 @@ import sys
 from fbchat import Client
 from fbchat.models import *
 
+class MessageListener(Client):
+    def onMessage(self, author_id, message_object, thread_id, thread_type, **kwargs):
+        print(message_object.text)
+
+
 
 if(len(sys.argv) >= 3):
     uname = sys.argv[1]
     pword = sys.argv[2]
-    client = Client(uname, pword)
+    #client = Client(uname, pword)
+    client = MessageListener(uname, pword)
 
     if not client.isLoggedIn():
         print "Login Failed!"
     else:
         print('My ID: ' + client.uid)
+        threads = client.fetchThreadList()
+        client.listen()
+
         
 
 else:
